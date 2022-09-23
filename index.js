bc777c3b7e3cd9851b50bfdea0f90525bc17e9ba
aryasalehi <64514807+aryasalehi@users.noreply.github.com>
2022-09-23 15:40:02 +0330

    add skylight cycle

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import * as THREE from 'three';
 import {Sky} from './Sky.js';
 import metaversefile from 'metaversefile';
-const {useApp, useFrame, useLocalPlayer} = metaversefile;
+const {useApp, useFrame, useLocalPlayer, useSkyManager} = metaversefile;
+
+const skyLightPosition = new THREE.Vector3();
+const sunPosition = new THREE.Vector3();
+const moonPosition = new THREE.Vector3();
 
 export default () => {
   const app = useApp();
-  
+  const localPlayer = useLocalPlayer();
+  const skyManager = useSkyManager();
+
   const effectController = {
     turbidity: 20,
     rayleigh: 10,
@@ -15,32 +21,60 @@ export default () => {
     azimuth: 0, // Facing front,
     // exposure: renderer.toneMappingExposure
   };
-  const sun = new THREE.Vector3();
+
+  const ambientLight = new THREE.AmbientLight('#fff', 0.5);
+  app.add(ambientLight);
+
+  const skyLight = skyManager.getSkyLight();
+
+  app.add(skyLight);
+  app.add(skyLight.target);
+
+  const skybox = new Sky();
+  skybox.scale.setScalar(10000);
+  app.add(skybox);
+  skybox.updateMatrixWorld();
+  app.setComponent('renderPriority', 'low');
+
   useFrame(() => {
-    var uniforms = skybox.material.uniforms;
+    // ?* moves the skybox app so that player never passes the skybox's walls
+    app.position.copy(localPlayer.position);
+    app.updateMatrixWorld();
+
+    // uniforms
+    const uniforms = skybox.material.uniforms;
     uniforms.turbidity.value = effectController.turbidity;
     uniforms.rayleigh.value = effectController.rayleigh;
     uniforms.mieCoefficient.value = effectController.mieCoefficient;
     uniforms.mieDirectionalG.value = effectController.mieDirectionalG;
 
-    const {position} = useLocalPlayer().leftHand;
-    // effectController.azimuth = (0.05 + ((Date.now() / 1000) * 0.1)) % 1;
-    effectController.azimuth = Math.min(Math.max(-position.z / 30, -0.3), 0.3);
-    var theta = Math.PI * (effectController.inclination - 0.5);
-    var phi = 2 * Math.PI * (effectController.azimuth - 0.5);
+    // const { position } = useLocalPlayer().leftHand;
+    effectController.azimuth = (0.05 + (Date.now() / 4000) * 0.1) % 1;
+    // effectController.azimuth = Math.min(Math.max(-position.z / 30, -0.3), 0.3);
+    const theta = Math.PI * (effectController.inclination - 0.5);
+    const phi = 2 * Math.PI * (effectController.azimuth - 0.5);
 
-    sun.x = Math.cos(phi);
-    sun.y = Math.sin(phi) * Math.sin(theta);
-    sun.z = Math.sin(phi) * Math.cos(theta);
+    sunPosition.x = Math.cos(phi);
+    sunPosition.y = Math.sin(phi) * Math.sin(theta);
+    sunPosition.z = Math.sin(phi) * Math.cos(theta);
 
-    uniforms.sunPosition.value.copy(sun);
+    uniforms.sunPosition.value.copy(sunPosition);
+
+    if (effectController.azimuth < 0.5) {
+      // sun
+      skyLightPosition.copy(sunPosition);
+      skyManager.setSkyLightColor('#fff');
+      skyManager.setSkyLightIntensity(6);
+    } else {
+      // moon
+      moonPosition.copy(sunPosition).multiplyScalar(-1);
+      skyLightPosition.copy(moonPosition);
+      skyManager.setSkyLightColor('#98caf5');
+      skyManager.setSkyLightIntensity(2);
+    }
+
+    skyManager.setSkyLightPosition(skyLightPosition);
   });
-
-  const skybox = new Sky();
-  skybox.scale.setScalar(1000);
-  app.add(skybox);
-  skybox.updateMatrixWorld();
-  app.setComponent('renderPriority', 'low');
 
   return app;
 };
